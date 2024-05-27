60d7dabce145bacc1518213f944474eb7341ae9f
Valentin Morlock <valentin@morlock.io>
2024-05-28 00:04:36 +0200

    qs

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,6 @@ fn main() {
     let mut pin = Gpio::new().unwrap().get(23).unwrap().into_input();
     loop {
         println!("{:?}", pin.is_high());
-        sleep(Duration::from_micros(10));
+        sleep(Duration::from_millis(10));
     }
 }
